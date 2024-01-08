94dc17e916544ee6391b6410f7d70fdbb7da8181
vgtoui <toni.uimonen@tuni.fi>
2024-01-08 10:30:27 +0200

    Create a Spring Boot project

---
+++ src/main/java/com/uicode/tcproject/TcProjectApplication.java
@@ -0,0 +1,13 @@
+package com.uicode.tcproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uicode/tcproject/TcProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uicode.tcproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
