0fdaabbcae1396447ebde68f94fca65dfc9ce045
vgtoui <toni.uimonen@tuni.fi>
2024-01-08 14:53:02 +0200

    Create user entity

---
+++ src/main/java/com/uicode/tcproject/entity/User.java
@@ -0,0 +1,25 @@
+package com.uicode.tcproject.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "user")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    String id;
+
+    @Column(name = "first_name")
+    String firstName;
+
+    @Column(name = "last_name")
+    String lastName;
+
+    @Column(name = "email")
+    String email;
+
+}
