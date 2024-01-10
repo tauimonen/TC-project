9d176534b26b614402f669e5f2309267ed730efb
vgtoui <toni.uimonen@tuni.fi>
2024-01-10 11:18:11 +0200

    Edit readme

--- src/main/java/com/uicode/tcproject/config/MyDataRestConfig.java
+++ src/main/java/com/uicode/tcproject/config/MyDataRestConfig.java
@@ -1,5 +1,7 @@
 package com.uicode.tcproject.config;
 
+import com.uicode.tcproject.entity.Exercise;
+import com.uicode.tcproject.entity.User;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -11,6 +13,9 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config,
                                                      CorsRegistry cors) {
+        config.exposeIdsFor(User.class);
+        config.exposeIdsFor(Exercise.class);
+
         /* Configure CORS Mapping */
         cors.addMapping(config.getBasePath() + "/**")
                 .allowedOrigins(theAllowedOrigins);
