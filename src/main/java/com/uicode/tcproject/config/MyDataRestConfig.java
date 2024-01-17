9ff3286da3585e272a6b82b9064541120ba93324
vgtoui <toni.uimonen@tuni.fi>
2024-01-17 16:49:08 +0200

    Create findExercisesByUserId query to ExerciseRepository

--- src/main/java/com/uicode/tcproject/config/MyDataRestConfig.java
+++ src/main/java/com/uicode/tcproject/config/MyDataRestConfig.java
@@ -2,10 +2,12 @@ package com.uicode.tcproject.config;
 
 import com.uicode.tcproject.entity.Exercise;
 import com.uicode.tcproject.entity.User;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+@Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
 
     private String theAllowedOrigins = "http://localhost:3000";
--- src/main/java/com/uicode/tcproject/dao/ExerciseRepository.java
+++ src/main/java/com/uicode/tcproject/dao/ExerciseRepository.java
@@ -2,7 +2,14 @@ package com.uicode.tcproject.dao;
 
 import com.uicode.tcproject.entity.Exercise;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.awt.print.Book;
+import java.util.List;
 
 public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
 
+    @Query("SELECT o FROM Exercise o WHERE userId = :user_id")
+    List<Exercise> findExercisesByUserId(@Param("user_id") Long userId);
 }
